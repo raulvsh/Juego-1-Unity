d0bc3b28e1379d59efdd5fc804ca08bed5c1660b
raulvsh <raulvsh@gmail.com>
2021-01-22 00:30:08 +0100

    Añadida furgoneta, barriles, gravedad, script movimiento

---
+++ Assets/Scripts/ControlJugador.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlJugador : MonoBehaviour
+{
+    int speed = 15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mover la furgoneta hacia delante
+        //transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+    }
+}
